5828177bb4bdc111f95f0227f273c8d82ab70b4a
swift3 <handleftman@gmail.com>
2023-02-11 23:55:53 +0300

    -fix gitignore -delete iml file -delete comment -hide function on class ManagerService (not use for #1 task)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,22 +16,6 @@ import java.util.Scanner
 fun main(args: Array<String>) {
 
     startParking()
-
-    //val scanner = Scanner(System.`in`)
-    //println("Добро пожаловать в систему учёта парковки автомобилей")
-    //println("Для начала работы программы введите команду /start")
-
-    /*while (scanner.hasNextLine()) {
-        when(scanner.nextLine()) {
-            "/start" -> {
-                println("Запуск программы по обслуживанию парковки. Пожалуйста введите команду. Для помощи используйте /help")
-                startParking()
-            }
-            "/help" -> println("печатаем спраку help")
-            else -> println("программа не может обработать запрос, введите /help для помощи")
-        }
-    }*/
-
 }
 
 private fun startParking() {
@@ -61,32 +45,6 @@ private fun startParking() {
                 println("Завершение работы.")
                 break
             }
-
-            /*Commander.PARK -> {
-                if (verifityCommand(cmd, 7)) {
-                    println("park command")
-                    manager.parking(cmd)
-                } else {
-                    println("park command wrong")
-                    continue
-                }
-            }
-
-            Commander.SHOW_PARKING -> {
-                println("show parking command")
-                manager.showParking(cmd)
-            }
-
-            Commander.SHOW_STATS -> {
-                println("show stats command")
-                manager.showStats(cmd)
-            }
-
-            Commander.RETURN -> {
-                println("return command")
-            }
-
-           */
 
             Commander.ERROR -> println("Команда не найдена, пожалуйста обратитесь к документации или наберите /help")
             else -> println("Команда не найдена, пожалуйста обратитесь к документации или наберите /help")
--- src/main/kotlin/Parking.kt
+++ src/main/kotlin/Parking.kt
@@ -14,5 +14,4 @@ object Parking {
         return parkingList.filterValues { it == null }.keys.firstOrNull()
     }
 
-
 }
